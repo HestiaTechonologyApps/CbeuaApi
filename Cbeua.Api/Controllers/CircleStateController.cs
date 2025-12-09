472733a52821cfce2fec9c7d1fc6ce4dd94eb576
aryananda@hestiatechnology.com <aryananda@hestiatechnology.com>
2025-12-09 17:45:53 +0530

    addedCircleState

---
+++ Cbeua.Api/Controllers/CircleStateController.cs
@@ -0,0 +1,130 @@
+﻿using Cbeua.Domain.DTO;
+using Cbeua.Domain.Entities;
+using Cbeua.Domain.Interfaces.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cbeua.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleStateController : ControllerBase
+    {
+        private readonly ICircleStateService _service;
+        public CircleStateController(ICircleStateService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<CustomApiResponse> GetAll()
+        {
+            var response = new CustomApiResponse();
+            try
+            {
+                var circleStates = await _service.GetAllAsync();
+                response.IsSucess = true;
+                response.Value = circleStates;
+                response.StatusCode = 200;
+            }
+            catch (Exception ex)
+            {
+                response.IsSucess = false;
+                response.Error = ex.Message;
+                response.StatusCode = 500;
+            }
+            return response;
+        }
+        [HttpGet("{id}")]
+        public async Task<CustomApiResponse> GetById(int id)
+        {
+            var response = new CustomApiResponse();
+            var circleState = await _service.GetByIdAsync(id);
+            if (circleState == null)
+            {
+                response.IsSucess = false;
+                response.Error = "Not found";
+                response.StatusCode = 404;
+            }
+            else
+            {
+                response.IsSucess = true;
+                response.Value = circleState;
+                response.StatusCode = 200;
+            }
+            return response;
+        }
+
+
+
+
+        [HttpPost]
+        public async Task<CustomApiResponse> Create([FromBody] CircleState circleState)
+        {
+            var response = new CustomApiResponse();
+            try
+            {
+                var created = await _service.CreateAsync(circleState);
+                response.IsSucess = true;
+                response.Value = created;
+                response.StatusCode = 201;
+            }
+            catch (Exception ex)
+            {
+                response.IsSucess = false;
+                response.Error = ex.Message;
+                response.StatusCode = 500;
+            }
+            return response;
+        }
+        [HttpPut("{id}")]
+        public async Task<CustomApiResponse> Update(int id, [FromBody] CircleState circleState)
+        {
+            var response = new CustomApiResponse();
+
+            if (id != circleState.CircleId)
+            {
+                response.IsSucess = false;
+                response.Error = "Id mismatch";
+                response.StatusCode = 400;
+                return response;
+            }
+
+            var updated = await _service.UpdateAsync(circleState);
+            if (!updated)
+            {
+                response.IsSucess = false;
+                response.Error = "Not found";
+                response.StatusCode = 404;
+            }
+            else
+            {
+                response.IsSucess = true;
+                response.Value = circleState;
+                response.StatusCode = 200;
+            }
+            return response;
+
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<CustomApiResponse> Delete(int id)
+        {
+            var response = new CustomApiResponse();
+            var deleted = await _service.DeleteAsync(id);
+            if (!deleted)
+            {
+                response.IsSucess = false;
+                response.Error = "Not found";
+                response.StatusCode = 404;
+            }
+            else
+            {
+                response.IsSucess = true;
+                response.Value = null;
+                response.StatusCode = 204;
+            }
+            return response;
+        }
+
+    }
+}
