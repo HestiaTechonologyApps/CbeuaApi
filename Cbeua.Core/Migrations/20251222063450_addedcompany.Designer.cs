36d0c5da34499550ad1dcdd36c2d6cbf96affc6d
aryananda@hestiatechnology.com <aryananda@hestiatechnology.com>
2025-12-22 12:07:23 +0530

    addedmigration

--- Cbeua.Core/Migrations/20251222063450_addedcompany.Designer.cs
+++ Cbeua.Core/Migrations/20251222063450_addedcompany.Designer.cs
@@ -4,6 +4,7 @@ using Cbeua.InfraCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cbeua.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251222063450_addedcompany")]
+    partial class addedcompany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Cbeua.Core/Migrations/20251222063450_addedcompany.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cbeua.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedcompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MainPages_CompanyId",
+                table: "MainPages",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MainPages_Companies_CompanyId",
+                table: "MainPages",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MainPages_Companies_CompanyId",
+                table: "MainPages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MainPages_CompanyId",
+                table: "MainPages");
+        }
+    }
+}
