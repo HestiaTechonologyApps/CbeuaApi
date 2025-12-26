be83c99f6b3997c096609af2e6b3f58779feaa67
sreenathganga1988 <sreenath.sreeganga@gmail.com>
2025-12-26 09:41:20 +0530

    corrected migration issue

--- Cbeua.Core/Migrations/20251222063450_addedcompany.cs
+++ Cbeua.Core/Migrations/20251222063450_addedcompany.cs
@@ -10,30 +10,30 @@ namespace Cbeua.Core.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateIndex(
-                name: "IX_MainPages_CompanyId",
-                table: "MainPages",
-                column: "CompanyId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_MainPages_CompanyId",
+            //    table: "MainPages",
+            //    column: "CompanyId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_MainPages_Companies_CompanyId",
-                table: "MainPages",
-                column: "CompanyId",
-                principalTable: "Companies",
-                principalColumn: "CompanyId",
-                onDelete: ReferentialAction.Cascade);
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_MainPages_Companies_CompanyId",
+            //    table: "MainPages",
+            //    column: "CompanyId",
+            //    principalTable: "Companies",
+            //    principalColumn: "CompanyId",
+            //    onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_MainPages_Companies_CompanyId",
-                table: "MainPages");
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_MainPages_Companies_CompanyId",
+            //    table: "MainPages");
 
-            migrationBuilder.DropIndex(
-                name: "IX_MainPages_CompanyId",
-                table: "MainPages");
+            //migrationBuilder.DropIndex(
+            //    name: "IX_MainPages_CompanyId",
+            //    table: "MainPages");
         }
     }
 }
