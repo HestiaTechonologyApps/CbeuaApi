d06e50d48bd07deb238ed00fc57e0efc4af60066
shima-p-ashok <shimapashok29@gmail.com>
2026-02-04 14:49:54 +0530

    publicpage

---
+++ Cbeua.Core/Migrations/20260204091415_publicpagefields.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cbeua.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class publicpagefields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Circles_States_StateId",
+                table: "Circles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Circles_StateId",
+                table: "Circles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FooterQuickHead",
+                table: "publicPages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManagingCommitteeHeaderSubTitle",
+                table: "publicPages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManagingCommitteeHeaderTitle",
+                table: "publicPages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FooterQuickHead",
+                table: "publicPages");
+
+            migrationBuilder.DropColumn(
+                name: "ManagingCommitteeHeaderSubTitle",
+                table: "publicPages");
+
+            migrationBuilder.DropColumn(
+                name: "ManagingCommitteeHeaderTitle",
+                table: "publicPages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Circles_StateId",
+                table: "Circles",
+                column: "StateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Circles_States_StateId",
+                table: "Circles",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "StateId");
+        }
+    }
+}
