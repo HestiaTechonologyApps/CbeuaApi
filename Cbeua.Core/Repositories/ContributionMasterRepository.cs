dffbebf8442feb5b4891cb158f9a397cbc1c90a4
AswinMahesh1 <aswin@hestiatechnology.com>
2026-05-11 10:07:37 +0530

    getall approved

--- Cbeua.Core/Repositories/ContributionMasterRepository.cs
+++ Cbeua.Core/Repositories/ContributionMasterRepository.cs
@@ -22,7 +22,8 @@ namespace Cbeua.Core.Repositories
         public async Task<List<ContributionMaster>> GetAllAsync()
         {
             return await _context.ContributionMasters
-                .Where(m => m.ContributionStatus.Trim().ToUpper() == "FORWARD")
+                .Where(m => m.ContributionStatus.Trim().ToUpper() == "FORWARD" ||
+                            m.ContributionStatus.Trim().ToUpper() == "A")
                 .OrderByDescending(m => m.Year)
                 .ThenByDescending(m => m.Month)
                 .ToListAsync();
