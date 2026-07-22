dd8710b1a9723ddf3e15a4ae5200eac24b0e76b6
shima-p-ashok <shimapashok29@gmail.com>
2026-07-22 10:11:28 +0530

    migration-DirectPayment

---
+++ Cbeua.Core/Migrations/20260722043800_Directpayment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cbeua.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Directpayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedBy",
+                table: "DirectPayments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ApprovedDate",
+                table: "DirectPayments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isApproved",
+                table: "DirectPayments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "DirectPayments");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovedDate",
+                table: "DirectPayments");
+
+            migrationBuilder.DropColumn(
+                name: "isApproved",
+                table: "DirectPayments");
+        }
+    }
+}
